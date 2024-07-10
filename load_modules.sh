e593cde26839af8a131f41d7509628a50fef6a28
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-07-10 16:44:10 +0100

    modules to load on Baskerville

---
+++ load_modules.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+module load Python/3.10.4-GCCcore-11.3.0
+module load OpenBLAS/0.3.20-GCC-11.3.0
+module load ScaLAPACK/2.2.0-gompi-2022a-fb
+module load HDF5/1.12.2-gompi-2022a
+module load OpenMPI/4.1.4-GCC-11.3.0
+module load FFTW/3.3.10-GCC-11.3.0 
