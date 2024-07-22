c7cae84c5c97f57f63db0f88382443f65df107e3
ots22 <ostrickson@turing.ac.uk>
2024-07-22 17:04:50 +0100

    Downgrade to gcc 11.3.0

--- load_modules.sh
+++ load_modules.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-module load Python/3.11.3-GCCcore-12.3.0
-module load OpenMPI/4.1.5-GCC-12.3.0
-module load FFTW/3.3.10-GCC-12.3.0
-
+module load FFTW/3.3.10-GCC-11.3.0
+module load OpenMPI/4.1.4-GCC-11.3.0
+module load Python/3.10.4-GCCcore-11.3.0
