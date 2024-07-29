d331252b3fe6ac483264ad9839d9dc4a77e2a062
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-07-29 13:10:18 +0100

    update script to plot POD results

--- plotting/plot_POD.py
+++ plotting/plot_POD.py
@@ -1,12 +1,27 @@
+"""
+This script plots:
+    1. The POD spatial modes on a world map
+    2. The temporal evolution of the POD modes
+    3. The cumulative energy of the POD modes
+The plots are saved in the `figs/` folder.
+The script requires the following files:
+    - The dataset file `dataset_{variable}.h5`
+    - The POD file `POD_{variable}.h5`
+"""
+
 import pyLOM
 import matplotlib.pyplot as plt
 import numpy as np
 import MapPlotter as mp
 
-## Input
-variable = 'hourly'
-modes    = np.arange(20)
+font = {'weight': 'bold', 'size': 12}
+plt.rc('font', **font)
+
+## User Inputs
+variable = 'daily'  # "hourly", "daily", "monthly"
+modes    = np.arange(20)  # Modes to plot
 prlevel  = 0
+x_label_temporal = 'Time [days]'
 
 ## Dataset load
 d = pyLOM.Dataset.load('dataset_%s.h5'%variable)
@@ -19,8 +34,6 @@ nlon = lon.shape[0]
 
 ## Load POD
 U, S, V = pyLOM.POD.load('POD_%s.h5' % variable,ptable=d.partition_table)
-
-np.save('V_%s.npy' % variable, V)
 
 for mode in modes:
     ## Plot data
@@ -43,14 +56,18 @@ for mode in modes:
     params['tick_format'] = '%.2e'
     params['bounds']     = [-np.max(np.abs(varplot)), np.max(np.abs(varplot))]
     plotter.plot(lon, lat, varplot, params=params)
-    plotter.save('mode_%i_%s.png' % (mode, variable))
+    plotter.save('figs/mode_%i_%s.png' % (mode, variable))
+    plotter.close()
+
 
     plt.figure(figsize=(8,6))
-    plt.plot(V[mode,:],'b', linewidth=2, label='Mode %i'% (mode))
-    plt.xlabel('Time [hours]')
+    plt.plot(V[mode,:],'g', linewidth=2, label='Mode %i'% (mode))
+    plt.xlabel(x_label_temporal)
     plt.ylabel(r'$V_i$')
     plt.title('Mode %i' %(mode))
-    plt.savefig('temporal_%i_%s.png'%(mode,variable), dpi=300)
+    plt.tight_layout
+    plt.savefig('figs/temporal_%i_%s.pdf' % (mode,variable), dpi=300)
+    plt.close()
 
 energy = np.cumsum(S**2)/np.sum(S**2)
 print(np.argwhere(energy>0.99)[0,0])
@@ -58,7 +75,8 @@ plt.figure()
 plt.plot(np.cumsum(S**2)/np.sum(S**2),'bo')
 plt.xlabel('Mode')
 plt.ylabel('Cumulative energy')
-plt.savefig('energy_%s.png'%variable, dpi=300)
+plt.savefig('figs/energy_%s.png'%variable, dpi=300)
+plt.close()
 
 #plt.show()
 
