f4f968fd4f814a37e54f64de62caa6a90373f67f
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-07-29 13:14:09 +0100

    update script to plot PSD estimate

--- plotting/plot_PSD.py
+++ plotting/plot_PSD.py
@@ -1,32 +1,38 @@
+"""
+Script to plot the Power Spectral Density (PSD) of the POD modes temporal evolution.
+The PSD is estimated using the Welch method.
+The plots are saved in the `figs/` folder.
+
+The script requires the following files:
+    - The POD file `POD_{variable}.h5`
+"""
+
 from scipy.signal import welch
 import numpy as np
 import matplotlib.pyplot as plt
+import pyLOM
 
+## User Inputs
+variable = 'daily'  # "hourly", "daily", "monthly"
+modes = np.arange(10) # Modes to plot
+fs = 1/24  # Sampling frequency (e.g. for daily, fs=1/24 - 1 sample every 24 hours)
+x_label = 'Frequency [1/hour]'
 
-# load .npy file
-V = np.load('V_daily.npy')
+## Load POD
+_, _, V = pyLOM.POD.load('POD_%s.h5' % variable)
 
-# compute PSD. The sampling frequency is 1 sample per 24 hours
-fs = 1/24
-mode = 1
-f, Pxx = welch(V[mode, :], fs)
+for mode in modes:
+    # compute PSD
+    f, Pxx = welch(V[mode, :], fs)
 
-# plot in log-log scale
-fig, ax = plt.subplots()
-ax.loglog(f, Pxx)
-ax.set_xlabel('Frequency [1/hour]')
-ax.set_ylabel('PSD')
-ax.set_title(f'PSD of mode {mode}')
+    # plot in log-log scale
+    fig, ax = plt.subplots()
+    ax.loglog(f, Pxx)
+    ax.set_xlabel('Frequency [1/hour]')
+    ax.set_ylabel('PSD')
+    ax.set_title(f'PSD of mode {mode}')
 
-# save figure
-plt.savefig(f'PSD_{mode}.png')
-
-# plot time series
-fig, ax = plt.subplots()
-ax.plot(V[mode, :])
-ax.set_xlabel('Time [days]')
-ax.set_title(f'Mode {mode} time series')
-
-# save figure
-plt.savefig(f'time_series_{mode}.png')
+    # save figure
+    plt.savefig(f'figs/PSD_{mode}.png')
+    plt.close()
 
