1a39cedaf3cc6532c4aa4977264a29965580809b
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-08-01 11:58:12 +0100

    minor edits to script to run POD on ERA5

--- Examples/run_POD_surface.py
+++ Examples/run_POD_surface.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
-#
-# POD of ERA5.
-#
-# Last revision: 15/07/2024
+
+"""
+Run POD on ERA5 data.
+The data is loaded from a .h5 file and the POD results are saved in a new .h5 file.
+
+Udpate the user inputs as needed before running the script.
+"""
+
 from __future__ import print_function, division
 
 import mpi4py
@@ -11,10 +15,11 @@ mpi4py.rc.recv_mprobe = False
 import os, numpy as np
 import pyLOM
 
-
-## Parameters
-DATAFILE = 'dataset.h5'
+## User inputs
+freq = "monthly"
+DATAFILE = f'dataset_{freq}.h5'
 VARLIST  = ['temperature']
+
 ## Data loadingx
 pyLOM.pprint(0, 'Loading data...', flush=True)
 d = pyLOM.Dataset.load(DATAFILE)
@@ -28,7 +33,7 @@ pyLOM.pprint(0, 'POD done!', flush=True)
 
 ## Save POD
 pyLOM.pprint(0, 'Saving .h5 ...', flush=True)
-pyLOM.POD.save('POD.h5',PSI,S,V,d.partition_table,nvars=1,pointData=True)
+pyLOM.POD.save(f'POD_{freq}.h5',PSI,S,V,d.partition_table,nvars=1,pointData=True)
 pyLOM.pprint(0, 'h5 saved!', flush=True)
 
 pyLOM.cr_info()
