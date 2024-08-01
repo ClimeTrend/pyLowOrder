25d37c0ff41c80e7c5a44782f1b0bf51c500ccf0
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2024-08-01 12:35:54 +0100

    plot PSD both with periodogram and welch

--- plotting/plot_PSD.py
+++ plotting/plot_PSD.py
@@ -9,17 +9,16 @@ The script requires the following files:
 Make sure to update the user inputs as needed.
 """
 
-from scipy.signal import welch
+from scipy.signal import welch, periodogram
 import numpy as np
 import matplotlib.pyplot as plt
 import pyLOM
 
 ## User Inputs
 variable = 'daily'  # "hourly", "daily", "monthly"
-modes = np.arange(10) # Modes to plot
+modes = np.arange(5) # Modes to plot
 fs = 1/24  # Sampling frequency (e.g. for daily, fs=1/24 - 1 sample every 24 hours)
 x_label = 'Frequency [1/hour]'
-color = 'g'  # convention is: monthly -> 'r', daily -> 'g', hourly -> 'b'
 
 ## Plotting settings
 font = {'weight': 'bold', 'size': 12}
@@ -30,14 +29,19 @@ _, _, V = pyLOM.POD.load('POD_%s.h5' % variable)
 
 for mode in modes:
     # compute PSD
-    f, Pxx = welch(V[mode, :], fs)
+    f, Pxx = periodogram(V[mode, :], fs)
+    f_welch, Pxx_welch = welch(V[mode, :], fs)
 
     # plot in log-log scale
     fig, ax = plt.subplots()
-    ax.loglog(f, Pxx, color=color)
+    ax.loglog(f, Pxx, color='blue', label='Periodogram')
+    ax.loglog(f_welch, Pxx_welch, color='red', label='Welch')
+    plt.ylim(1e-6, 1e2)
+    plt.grid(which='both', axis='x')
     ax.set_xlabel('Frequency [1/hour]')
     ax.set_ylabel('PSD')
     ax.set_title(f'PSD of mode {mode}')
+    ax.legend()
 
     # save figure
     plt.savefig(f'figs/PSD_{mode}_{variable}.pdf', dpi=300)
